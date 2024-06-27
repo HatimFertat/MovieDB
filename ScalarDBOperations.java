7983c431ec7466b99036f99100f0487a1d43ed96
HatimFertat <hatimfertat2001@gmail.com>
2024-06-27 20:58:51 +0900

    prevents from adding if the movie is already in the list

--- ScalarDBOperations.java
+++ ScalarDBOperations.java
@@ -15,6 +15,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 
 public class ScalarDBOperations {
@@ -35,28 +36,56 @@ public class ScalarDBOperations {
             throw new RuntimeException("Initialization of ScalarDB failed.", e);
         }
     }
-    
+
+    public static boolean isMovieInList(int userId, int movieId, String listName) throws TransactionException {
+        DistributedTransaction tx = manager.start();
+        try {
+            Get getMovie = Get.newBuilder()
+                .namespace(NAMESPACE)
+                .table(listName)
+                .partitionKey(Key.ofInt("userId", userId))
+                .clusteringKey(Key.ofInt("movieId", movieId))
+                .build();
+
+            Optional<Result> result = tx.get(getMovie);
+            tx.commit();
+
+            return result.isPresent();
+        } catch (Exception e) {
+            tx.rollback();
+            throw new TransactionException("Failed to check if movie is in " + listName + ": ", e.getMessage());
+        }
+    }
 
     public static void addMovieToList(int userId, Map<String, String> movieDetails, String listName) throws TransactionException {
+        if (isMovieInList(userId, Integer.parseInt(movieDetails.get("movieId")), listName)) {
+            System.out.println("Movie is already in " + listName);
+            return;
+        }
+
         DistributedTransaction tx = manager.start();
         try {
             String moviesTable = "movies";
 
-            Put putMovie = new Put(new Key("movieId", Integer.parseInt(movieDetails.get("movieId"))))
-                .forNamespace(NAMESPACE)
-                .forTable(moviesTable)
-                .withValue("title", movieDetails.get("title"))
-                .withValue("release_date", movieDetails.get("release_date"))
-                .withValue("director", movieDetails.get("director"))
-                .withValue("cast", movieDetails.get("cast"))
-                .withValue("synopsis", movieDetails.get("synopsis"))
-                .withValue("genres", movieDetails.get("genres"));
-            tx.put(putMovie);
+            Put putMovie = Put.newBuilder()
+                .namespace(NAMESPACE)
+                .table(moviesTable)
+                .partitionKey(Key.ofInt("movieId", Integer.parseInt(movieDetails.get("movieId"))))
+                .textValue("title", movieDetails.get("title"))
+                .textValue("release_date", movieDetails.get("release_date"))
+                .textValue("director", movieDetails.get("director"))
+                .textValue("cast", movieDetails.get("cast"))
+                .textValue("synopsis", movieDetails.get("synopsis"))
+                .textValue("genres", movieDetails.get("genres"))
+                .build();                 
+                tx.put(putMovie);
     
-            Put putList = new Put(new Key("watchlistId", userId))
-                .forNamespace(NAMESPACE)
-                .forTable(listName)
-                .withValue("movie_id", Integer.parseInt(movieDetails.get("movieId")));
+            Put putList = Put.newBuilder()
+                .namespace(NAMESPACE)
+                .table(listName)
+                .partitionKey(Key.ofInt("userId", userId))
+                .clusteringKey(Key.ofInt("movieId", Integer.parseInt(movieDetails.get("movieId"))))
+                .build();
             tx.put(putList);
     
             tx.commit();
